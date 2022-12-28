394d44e9ee4ef139af12e0b7b542ebaa3e896e39
mysterlee418 <mysterlee418@gmail.com>
2022-12-28 17:26:45 +0900

    12/28

--- WhatisFunction/FunctionTraining.cs
+++ WhatisFunction/FunctionTraining.cs
@@ -7,9 +7,43 @@ namespace WhatisFunction {
 
             SayHello(3);
             Console.WriteLine("최대 값은 : {0}", Maximum(10, 8, 3));
-            Console.WriteLine("직각 삼각형 빗변의 길이는 : {0}", Hypot(1, 2));
+            Console.WriteLine("직각 삼각형 빗변의 길이는 : {0:F4}", Hypot(1.2, 2.8));
+            Console.WriteLine("직각 삼각형 빗변의 길이는 : {0}", Math.Round(Hypot(1.2, 2.8), 2));
+
             Prime();
             TelephoneNumber();
+
+
+            Console.Write("문자를 입력하시오.. ");
+            ReverseString(Console.ReadLine());
+
+
+
+        }
+
+        public static void ReverseString(String stringValue) {
+
+            char[] temp = new char[stringValue.Length];
+            int idx = 0;
+            
+
+            foreach (char character in stringValue) {
+                temp[idx] = character;
+                idx++;
+            }
+
+            for(int idx1 = temp.GetUpperBound(0); idx1 >= 0; idx1--) {
+                Console.Write(temp[idx1]);
+            }
+            
+            
+
+            
+
+
+
+
+
 
 
         }
@@ -81,6 +115,7 @@ namespace WhatisFunction {
 
             for (int i = 2; i < 100 + 1; i++) {
                 Boolean isPrime = checkPrime(i);
+                
                 if (isPrime) {
                     Console.Write(" {0} ", i);
                 }
